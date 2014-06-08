981da264873ef6c856ca81a30e954126c2d376a1
Andy Cook <cromulent11@gmail.com>
2014-06-08 18:49:13 -0500

    Removing leftover histogram

--- plot2.R
+++ plot2.R
@@ -2,7 +2,6 @@ library(data.table)
 testData <- data.table(read.table("./data//household_power_consumption.txt", sep = ";", header = TRUE, na.strings="?"))
 newTest <- testData[(as.Date(testData$Date, "%d/%m/%Y") == as.Date("01/02/2007", "%d/%m/%Y")) | (as.Date(testData$Date, "%d/%m/%Y") == as.Date("02/02/2007", "%d/%m/%Y")),]
 png(filename = "plot2.png")
-hist(newTest$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main="Global Active Power")
 xVals<-strptime(paste(newTest$Date, newTest$Time, sep=" "),format='%d/%m/%Y %H:%M:%S')
 yVals<-newTest$Global_active_power
 plot(xVals, yVals, type="l", xlab="", ylab="Global Active Power (kilowatts)", main="")
