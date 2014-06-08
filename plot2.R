f410217571b2e250df6e7ecdb7af0f503567d730
Andy Cook <cromulent11@gmail.com>
2014-06-08 18:35:53 -0500

    Plot2

--- plot2.R
+++ plot2.R
@@ -1,10 +1,9 @@
 library(data.table)
 testData <- data.table(read.table("./data//household_power_consumption.txt", sep = ";", header = TRUE, na.strings="?"))
-##df <- read.table("./data//household_power_consumption.txt", sep = ";", header = TRUE, na.strings="?")
 newTest <- testData[(as.Date(testData$Date, "%d/%m/%Y") == as.Date("01/02/2007", "%d/%m/%Y")) | (as.Date(testData$Date, "%d/%m/%Y") == as.Date("02/02/2007", "%d/%m/%Y")),]
-png(filename = "plot1.png")
+png(filename = "plot2.png")
 hist(newTest$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", main="Global Active Power")
-with(newTest, plot(strptime(paste(Date, Time, sep=" "),format='%d/%m/%Y %H:%M:%S'), 
-                   Global_active_power), type="l")
-##lines(x, y)
+xVals<-strptime(paste(newTest$Date, newTest$Time, sep=" "),format='%d/%m/%Y %H:%M:%S')
+yVals<-newTest$Global_active_power
+plot(xVals, yVals, type="l", xlab="", ylab="Global Active Power (kilowatts)", main="")
 dev.off()
